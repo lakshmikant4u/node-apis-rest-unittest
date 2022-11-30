eb2fed2a8860d77f847eb1f9cee51a034e6e8893
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-01 01:19:19 +0530

    change of folder

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -33,3 +33,18 @@ describe("Calculator Tests", () => {
         expect(result).toBe(3);
     });
 })
+
+describe("Testing with Jest", () => {
+    test("Addition", () => {
+        const sum = 2 + 3;
+        const expectedResult = 5;
+        expect(sum).toEqual(expectedResult);
+    });
+
+    // Jest also allows a test to run multiple
+    // times using different values
+    test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 5]])(
+        'Does %i + %i equals %i', (a, b, expectedResult) => {
+            expect(a + b).toBe(expectedResult);
+        });
+});
